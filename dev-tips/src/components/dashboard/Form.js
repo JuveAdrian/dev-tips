d306f7ee039ca47a67850f03186237c79799e937
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-06-07 00:33:25 +0200

    working with form

--- dev-tips/src/components/dashboard/Form.js
+++ dev-tips/src/components/dashboard/Form.js
@@ -1,20 +1,43 @@
 import React from 'react';
 
 class Form extends React.Component {
-    state = {
-        value: 'Testowy formularz'
-    }
-    handleChange(event) {
-        console.log(event.target.value);
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            value: 'Testowy formularz'
+        };
+
+        this.handleChange = (event) => {
+            console.log(event.target.value);
+            event.preventDefault()
+            const data = { title: this.state.value, content: 'example content', category: 'example category' };
+            fetch("http://localhost:4000/tips", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(data),
+                })
+                .then(response => response.json())
+                .then(data => {
+                  console.log('Success:', data);
+                })
+                .catch((error) => {
+                  console.error('Error:', error);
+                });
+      }
+    
+        
     }
     render() {
         return(
             <form onSubmit={this.handleSubmit}>
                 <label>
                     Imię:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    <input type="text" value={this.state.value}  />
                 </label>
-                <input type="submit" value="Wyślij" />
+                <input type="submit" onClick={this.handleChange}  value="Wyślij" />
             </form>
         )
     }
