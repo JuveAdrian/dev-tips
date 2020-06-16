4fa2faf32afd02be7d4d55496aca033121be70f8
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-06-16 14:06:44 +0200

    split code in userRouter to userController

---
+++ API/v1/users/userControllers.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const User = require('./userModel');
+
+module.exports = {
+    signup: (req, res, next) => {
+        User.findOne({ email: req.body.email })
+            .exec()
+            .then(user => {
+                if( user.length >= 1 ) {
+                    return res.status(409).json({
+                        message: 'Mail exists'
+                    });
+                } else {
+                    bycrypt.hash(req.body.password, 10, (err, hash) => {
+                        if(err) {
+                            console.log('halo');
+                            return res.status(500).json({
+                                error: err
+                            });
+                        } else {
+                            const user = new User({
+                                _id: new mongoose.Types.ObjectId(),
+                                email: req.body.email,
+                                password: hash
+                        });
+                        user
+                            .save()
+                            .then(result => {
+                                res.status(201).json({
+                                    message: 'User created'
+                                })
+                            })
+                            .catch(err => {
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                        }
+                    });
+                }
+            });   
+    },
+    deleteUser: (req, res, next) => {
+        User.deleteOne({_id: req.params.userid})
+            .exec()
+            .then(result => {
+                res.status(200).json({
+                    message: 'User deleted'
+                })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: err
+                });
+            });
+    }
+}
--- API/v1/users/userRouter.js
+++ API/v1/users/userRouter.js
@@ -4,61 +4,11 @@ const bycrypt = require('bcrypt');
 
 const router = express.Router();
 
+const userController = require('./userControllers');
 const User = require('../users/userModel');
 
-router.post('/signup', (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .exec()
-        .then(user => {
-            if( user.length >= 1 ) {
-                return res.status(409).json({
-                    message: 'Mail exists'
-                });
-            } else {
-                bycrypt.hash(req.body.password, 10, (err, hash) => {
-                    if(err) {
-                        console.log('halo');
-                        return res.status(500).json({
-                            error: err
-                        });
-                    } else {
-                        const user = new User({
-                            _id: new mongoose.Types.ObjectId(),
-                            email: req.body.email,
-                            password: hash
-                    });
-                    user
-                        .save()
-                        .then(result => {
-                            res.status(201).json({
-                                message: 'User created'
-                            })
-                        })
-                        .catch(err => {
-                            res.status(500).json({
-                                error: err
-                            });
-                        });
-                    }
-                });
-            }
-        });   
-});
+router.post('/signup', userController.signup);
 
-router.delete('/:userId', (req, res, next) => {
-    User.deleteOne({_id: req.params.userid})
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: 'User deleted'
-            })
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-});
+router.delete('/:userId', userController.deleteUser);
 
 module.exports = router;
