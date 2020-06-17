aa18428a15752ec597684d5a4ec1dd594d7c5759
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-06-17 22:36:25 +0200

    add jsonwebtoken package

--- API/v1/users/userRouter.js
+++ API/v1/users/userRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bycrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
