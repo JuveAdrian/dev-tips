6318fa535e55dd760ab71255ce335316f91111b7
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-06-17 22:28:22 +0200

    in api add login route

--- API/v1/users/userControllers.js
+++ API/v1/users/userControllers.js
@@ -55,5 +55,37 @@ module.exports = {
                     error: err
                 });
             });
+    },
+    login: (req, res, next) => {
+        User.find({ email: req.body.email })
+            .exec()
+            .then(user => {
+                if (user.length < 1) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    })
+                }
+                bycrypt.compare(req.body.password, user[0].password, (err, result) => {
+                    if (err) {
+                        return res.status(401).json({
+                            message: 'Auth failed'
+                        })
+                    }
+                    if (result) {
+                        return res.status(200).json({
+                            message: 'Auth successful'
+                        })
+                    }
+                    res.status(401).json({
+                        message: 'Auth failed'
+                    })
+                })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: err
+                });
+            })
     }
 }
--- API/v1/users/userRouter.js
+++ API/v1/users/userRouter.js
@@ -5,10 +5,9 @@ const bycrypt = require('bcrypt');
 const router = express.Router();
 
 const userController = require('./userControllers');
-const User = require('../users/userModel');
 
 router.post('/signup', userController.signup);
-
 router.delete('/:userId', userController.deleteUser);
+router.post('/login', userController.login);
 
 module.exports = router;
