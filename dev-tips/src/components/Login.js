a3e55a0f5bc241466954934050937fada8c2d3bd
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-06-19 23:04:42 +0200

    add Login component

---
+++ dev-tips/src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Strona logowania</h1>
+            </div>
+        )
+    }
+}
+
+export default Login;
