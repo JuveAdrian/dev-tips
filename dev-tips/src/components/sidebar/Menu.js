e3a92c2bc26d96cb15a5f2b1ad01e48890ccb45c
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-06-21 22:19:59 +0200

    simple menu styling

--- dev-tips/src/components/sidebar/Menu.js
+++ dev-tips/src/components/sidebar/Menu.js
@@ -4,7 +4,7 @@ class Menu extends React.Component {
     render() {
         return (
             <nav className="navbar navbar-default">
-                <div className="container">
+                <div className="container sidebarContainer">
                     <div className="navbar-header">
                         <ul className="nav navbar-nav">
                             <li><a href="">Home</a></li>
