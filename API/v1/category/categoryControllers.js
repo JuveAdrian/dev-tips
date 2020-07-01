8f8b0b59dfda78e7d0bb03e9fe194e82dac41daa
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-07-01 23:41:48 +0200

    add getAll categories controller

--- API/v1/category/categoryControllers.js
+++ API/v1/category/categoryControllers.js
@@ -4,7 +4,33 @@ const Category = require('./categoryModel');
 
 module.exports = {
     getAll: (req, res, next) => {
-        console.log('GET ALL');
+        Category.find()
+            .select()
+            .exec()
+            .then( docs => {
+                const response = {
+                    info: {
+                        count: docs.length
+                    },
+                    categories: docs.map(doc => {
+                        return {
+                            name: doc.name,
+                            _id: doc._id,
+                            request: {
+                                type: "GET",
+                                url: "http://localhost:4000/categories/" + doc._id
+                            }
+                        }
+                    })
+                }
+                res.status(200).json(response);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: err
+                });
+            });
     },
     getCategory: (req, res, next) => {
         console.log('GET ONE');
